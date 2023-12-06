90f619c7e85cc3da2e504b19799afd843ce80571
Dipankar Paul <31593497+paul-dipankar-cse@users.noreply.github.com>
2023-12-06 06:13:18 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict";       //treat all JS code as newer version
+
+//alert(3+3) // will give error as we are using nodejs not browser
+
+console.log(3
+    +3)     //code readability should be maintained
+
+let name = "Dipankar"       //string
+let age = 25                //integer   2 to the power 53
+let isLoggedIn = false      //boolean
+
+//null = standalone value
+//undefined = not assigned
+
+console.log(typeof undefined) //==> undefined
+console.log(typeof null);     //==> object
