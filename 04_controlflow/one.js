298e92e8e22abeb23c1f839649ff3fb957627085
Dipankar Paul <31593497+paul-dipankar-cse@users.noreply.github.com>
2023-12-17 17:18:16 +0000

    ifelse

---
+++ 04_controlflow/one.js
@@ -0,0 +1,24 @@
+//if
+
+if (true){
+    //will execute
+}
+
+if (2 == '2'){
+    console.log('Executed');
+}       //Executed
+
+if (2 === '2'){
+    console.log('Executed');
+}       //No Output
+
+if (false){
+    //will not execute
+}
+const temperature = 41
+if (temperature < 50){
+    console.log("Less than 50");
+}
+else{
+    console.log("Greater than 50");
+}
